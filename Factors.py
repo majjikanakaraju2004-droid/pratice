64a9ac9805c5782c4f9eb60f5a3af5b860d8017e
Kanaka Raju <majjikanakaraju2004@gmail.com>
2026-01-29 16:39:46 +0530

    .

--- Factors.py
+++ Factors.py
@@ -4,4 +4,4 @@ git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/majjikanakaraju2004-droid/pratice.git
-git push -u origin main
+git push -u 
