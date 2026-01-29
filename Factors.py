539a364c13daaec49c6e1a98d3815e3d48598f6d
M.J.R.K.KANAKA RAJU <majjikanakaraju2004@gmail.com>
2026-01-29 16:30:10 +0530

    Update Factors.py

--- Factors.py
+++ Factors.py
@@ -2,6 +2,6 @@ echo "# pratice" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
-git branch -M main
+git branch -M
 git remote add origin https://github.com/majjikanakaraju2004-droid/pratice.git
 git push -u origin main
